a57e66a1bd0ec4dc3a816c26f65e601917a4fee1
milosbugarinovic <milos.bugarinovic@gmail.com>
2023-12-03 20:34:19 +0100

    chore: esm and cjs build

--- resource/script/tsc/alias.sh
+++ resource/script/tsc/alias.sh
@@ -2,19 +2,23 @@
 
 # Initialize variables
 WATCH_MODE=0 # 0 means disabled, 1 means enabled
-
+PACKAGE_TYPE=esm
 # Function to display the usage of the script
 function display_usage {
-  echo "Usage: $0 [-w|--watch] project_name1 [project_name2 ...]"
+  echo "Usage: $0 [-w] [-c] project_name1 [project_name2 ...]"
   echo "Options:"
-  echo "  -w, --watch   Enable watch mode (recompile on file changes)."
+  echo "  -w   Enable watch mode (recompile on file changes)."
+  echo "  -c   Enable CommonJS alias paths."
 }
 
 # Parse command-line options using getopts
-while getopts ":w" opt; do
+while getopts ":w:c" opt; do
   case $opt in
     w)
       WATCH_MODE=1
+      ;;
+    c)
+      PACKAGE_TYPE=cjs
       ;;
     \?)
       echo "Error: Invalid option -$OPTARG" >&2
@@ -33,6 +37,6 @@ if [ $# -eq 0 ]; then
 fi
 
 for PROJECT_NAME in "$@"; do
-	echo "Register tsc-alias for $PROJECT_NAME"
-  npx tsc-alias -p "./packages/$PROJECT_NAME/tsconfig.json" "$([ $WATCH_MODE -eq 1 ] && echo '-w')" &
+	echo "Register tsc-alias for $PROJECT_NAME (options: watch=$WATCH_MODE, package_type=$PACKAGE_TYPE)"
+  npx tsc-alias -p "./packages/$PROJECT_NAME/tsconfig.$PACKAGE_TYPE.alias-paths.json" "$([ $WATCH_MODE -eq 1 ] && echo '-w')" &
 done
